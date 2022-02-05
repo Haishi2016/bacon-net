f7f5dfc0a827a4df7d4b68100825dc5f8e560239
Haishi Bai <hbai@microsoft.com>
2022-02-05 07:47:11 -0800

    doc updates, minor interface changes

--- src/nets/poly2.py
+++ src/nets/poly2.py
@@ -12,7 +12,11 @@ class poly2(baconNet):
     def get_contribution(self, a, b):
         return super().get_contribution(a, b)
 
-    def explain(self, a, b):
+    def explain(self, singleVariable=False):
+        a = 1
+        b = 1
+        if singleVariable:
+            b = 0
         m, c = self.get_contribution(a, b)
         delta = 0.01
         terms = []
@@ -22,13 +26,13 @@ class poly2(baconNet):
             terms.append(str(round(m[4][0], 4)) + "xy")
         if abs(m[3][0]) > delta and abs(b) > delta:
             terms.append(str(round(m[3][0], 4)) + "y^2")
-        if abs(m[0][0]) > delta and abs(b) > delta:
+        if abs(m[0][0]) > delta and abs(a) > delta:
             terms.append(str(round(m[0][0], 4)) + "x")
         if abs(m[1][0]) > delta and abs(b) > delta:
             terms.append(str(round(m[1][0], 4)) + "y")
         if abs(m[5][0]) > delta and abs(c) > delta:
             terms.append(str(round(c, 4)))
-        return "y = " + " + ".join(terms)
+        return "z = " + " + ".join(terms)
 
     def expand(self, a, b, y):
         X = np.column_stack((a, b,
