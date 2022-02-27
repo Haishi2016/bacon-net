e9b446eb942813d964b3dff0b4435d3adc870f52
Haishi Bai <hbai@microsoft.com>
2022-02-26 16:42:26 -0800

    new lsp implementation

--- samples/sample-apps/bacon-stack.py
+++ samples/sample-apps/bacon-stack.py
@@ -8,13 +8,15 @@ print("\nUsing a 3-variable poly2 stack ... \n")
 stack1 = baconStack(size=2, baconType="poly2")
 
 x1, y1 = dataCreator.create(
-    500, 1, lambda x: x[0] + x[1] + 2 * x[2], params=3)
-stack1.fit2(x1, y1, maeTarget=0.001, verbose=True, weightShift='random')
+    500, 1, lambda x: (x[0] + 3 * x[2]) * 2 * x[1], params=3)
+info = stack1.fit2(x1, y1, maeTarget=0.002, verbose=True, weightShift='random')
+print(
+    f"trained after {info['attempts']} tries, mae={info['mae']}, duration={info['duration']}")
 m1 = stack1.explain()
 print(m1.string(precision=2))
 print(m1.simplify(m1.string(precision=2)))
 
-paramCount = 5
+paramCount = 7
 
 print(f"\nUsing a {paramCount}-variable lsp3 stack ... \n")
 
@@ -24,9 +26,10 @@ stack2 = baconStack(size=paramCount-1, baconType="lsp3",
                     initializer='identity')
 
 x2, y2 = dataCreator.create(
-    1000, 1, lambda x: np.maximum(np.minimum(np.maximum(np.minimum(x[0], x[1]), x[2]), x[3]), x[4]), params=paramCount)
-history, mae, counter = stack2.fit2(
-    x2, y2, maeTarget=0.001, verbose=True, weightShift='random')
-print(f"trained after {counter} tries, mae={mae}")
-m2 = stack2.explain(delta=0.02)
+    500, 1, lambda x: np.maximum(np.maximum(np.minimum(np.maximum(np.minimum(np.maximum(x[0], x[1]), x[2]), x[3]), x[4]), x[5]), x[6]), params=paramCount)
+info = stack2.fit2(
+    x2, y2, maeTarget=0.005, verbose=True, weightShift='random', inputShift='off')
+print(
+    f"trained after {info['attempts']} tries, mae={info['mae']}, duration={info['duration']}")
+m2 = stack2.explain(delta=0.5)
 print(m2.string(ignoreOne=True, sympy=False))
