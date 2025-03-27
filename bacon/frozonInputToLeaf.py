735aeb7e1b677dc57d399c203ce207c31e9f2f43
Haishi Bai <hbai@microsoft.com>
2025-03-27 21:26:20 +0100

    do you believe it's 97.37%

---
+++ bacon/frozonInputToLeaf.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+class frozenInputToLeaf(nn.Module):
+    def __init__(self, hard_assignment, num_inputs):
+        super().__init__()
+        self.register_buffer("P_hard", torch.zeros(len(hard_assignment), num_inputs))
+        for leaf_idx, input_idx in enumerate(hard_assignment):
+            self.P_hard[leaf_idx, input_idx] = 1.0
+
+    def forward(self, x):
+        return torch.matmul(x, self.P_hard.t())  # Ensure correct device
